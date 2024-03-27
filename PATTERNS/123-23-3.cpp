0adc72609f8626fdd438750933f7b3cac666c568
Khushichauhan <kc6478576@gmail.com>
2024-03-27 12:52:58 +0530

    to do some changes

--- PATTERNS/123-23-3.cpp
+++ PATTERNS/123-23-3.cpp
@@ -1,26 +1,27 @@
 #include<iostream>
 using namespace std;
-int main() {
+int main(){
     int n;
-    cout << "Enter a number: ";
-    cin >> n;
+    cout<<"Enter a number :";
+    cin>>n;
 
     int i = 1;
-    while (i <= n) {
-        // Space print kar lo
-        int space = (i > 1) ? (i - 1) * 2 : 0; // Adjust space based on the line number
-        while (space) {
-            cout << " ";
+    while(i<=n){
+        //space print kar lo
+        int space = i - 1;
+        while(space){
+            cout<<" ";
             space = space - 1;
         }
-        // Number print kar lo
-        int j = i;
-        while (j <= n) {
-            cout << j << " ";
+        //star print kar lo
+        int j = 1;
+        while(j<=n-i+1){
+            cout<<i + j - 1;
             j++;
         }
-        cout << endl;
+        cout<<endl;
         i++;
     }
-    return 0;
+    
+
 }
